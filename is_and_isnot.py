efaa1907d0c32e07189bd16560676b94716877e2
adityasaivit <113006142+adityasaivit@users.noreply.github.com>
2023-04-10 01:47:14 +0530

    Create is_and_isnot.py

---
+++ is_and_isnot.py
@@ -0,0 +1,6 @@
+x=int(input("enter the value of x:"))
+y=int(input("enter the value of y:"))
+if(x is y):
+    print("both of the numbers are same")
+if x is not y:
+    print("both numbers are not same")
