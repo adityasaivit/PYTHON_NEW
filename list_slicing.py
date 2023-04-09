79359353cb37ed8a6fff24a72a1c03237e4257ce
adityasaivit <113006142+adityasaivit@users.noreply.github.com>
2023-04-10 02:12:49 +0530

    Add files via upload

---
+++ list_slicing.py
@@ -0,0 +1,9 @@
+list=["adithya","amrutha","srinivasarao","rajyalakshmi","adi","ammu"]
+print(list[1])
+print(list[len(list)-1])
+print(list[:3])
+print(list[3:])
+print(list[::2])
+print(list[-2])
+print(list[-4:-1])
+#same as the string start index included and end index in excluded
