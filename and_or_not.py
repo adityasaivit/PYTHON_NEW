b0dbf3930906011fc726cc03c01e499f2fb07012
adityasaivit <113006142+adityasaivit@users.noreply.github.com>
2023-04-10 01:43:48 +0530

    Create and_or_not.py

---
+++ and_or_not.py
@@ -0,0 +1,8 @@
+x=int(input("enter the value of x:"))
+y=int(input("enter the value of y:"))
+if(x==5 and y==2):#and operator
+    print("hello")
+if(x==5 or y==1):#or operator
+    print("hello or")
+if(not(x==6 and y==2)):#not operator
+    print("not operator")
